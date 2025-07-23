a05eabde69b208bf9817ee722b243af0e97d0142
Islam Mac <islam@mac.com>
2025-07-23 19:21:16 +0300

    admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -23,26 +23,104 @@
             <h6 class="mb-2 text-md font-bold tracking-tight text-gray-900 text-center"> قسم التكنولوجيا الطبية
                 الحيوية
             </h6>
-            <h6 class="mb-2 text-md font-bold tracking-tight text-gray-900 text-center"> تقرير الطالبين المسجلين
+            <h6 class="mb-2 text-md font-bold tracking-tight text-gray-900 text-center"> تقرير الطلاب المسجلين
             </h6>
         </div>
 
-        <section>
-            @foreach ($users as $user)
-                <div>{{ $user->studentName }}</div>
-                <div>{{ $user->email }}</div>
-                <div>{{ $user->mobile }}</div>
-                <div>{{ $user->type }}</div>
-                @foreach ($user->attachments as $attachment)
-                    <form action="{{ route('download') }}"  method="post">
-                        @csrf
-                        <input type="hidden" name="link" value="{{ $attachment->link }}">
-                        <button>{{ $attachment->readableName() }}</button>
-                    </form>
-                @endforeach
-                <hr>
-            @endforeach
-        </section>
+
+        @if ($users->isEmpty())
+            <div class="flex items-center p-4 mb-4 mx-5 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 "
+                role="alert">
+                <svg class="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span class="sr-only">Info</span>
+                <div class="font-medium text-lg">
+                    لا يوجد طلبه مسجلين حتي الآن
+                </div>
+            </div>
+        @else
+            <section>
+                {{-- @foreach ($users as $user)
+                    <div>{{ $user->studentName }}</div>
+                    <div>{{ $user->email }}</div>
+                    <div>{{ $user->mobile }}</div>
+                    <div>{{ $user->type }}</div>
+                    @foreach ($user->attachments as $attachment)
+                        <form action="{{ route('download') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="link" value="{{ $attachment->link }}">
+                            <button>{{ $attachment->readableName() }}</button>
+                        </form>
+                    @endforeach
+                    <hr>
+                @endforeach --}}
+
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 0">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-xl">
+                                    الطالب
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-xl">
+                                    الهوية
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-xl">
+                                    الجوال
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-xl">
+                                    الشهادة
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr class="bg-white border-gray-200 hover:bg-gray-600 hover:text-white">
+                                    <th scope="row"
+                                        class="flex items-center px-6 py-4  whitespace-nowrap ">
+                                        <div class="ps-3">
+                                            <div class="text-base font-semibold">{{ $user->studentName }}</div>
+                                            <div class="font-normal ">{{ $user->email }}</div>
+                                        </div>
+                                    </th>
+                                    <td class="px-6 py-4 text-lg">
+                                        {{ $user->nationalID }}
+                                    </td>
+                                    <td class="px-6 py-4 text-lg">
+                                        {{ $user->mobile }}
+                                    </td>
+                                    <td class="px-6 py-4 text-lg">
+                                        {{ $user->type }}
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b border-gray-200 hover:bg-gray-50">
+                                    <td colspan="4">
+                                        <div class="flex items-center py-4 mx-6 text-gray-900 whitespace-nowrap">
+                                            <div class="ps-3">
+                                                <div class="font-normal text-gray-500 flex flex-wrap gap-4 justify-between items-center">
+                                                    @foreach ($user->attachments as $attachment)
+                                                        <form action="{{ route('download') }}" method="post">
+                                                            @csrf
+                                                            <input type="hidden" name="link"
+                                                                value="{{ $attachment->link }}">
+                                                            <button class="bg-blue-500/50 hover:bg-blue-700 text-black/50 hover:text-white text-sm py-2 px-4 rounded-4xl">{{ $attachment->readableName() }}</button>
+                                                        </form>
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        @endif
+
     </div>
 </body>
 
